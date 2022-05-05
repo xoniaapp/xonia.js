df3d34bbd310db78e737f5a36a3bfae1f734d837
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-05 12:43:31 +0600

    change a few stuff

--- config.js
+++ config.js
@@ -1,7 +1,4 @@
-let baseURL = 'https://gateway.xoniaapp.com/api/';
-let sessionToken = 'xa=MTY0NTcxNDc2OHxOd3dBTkVsR1JrRldTMU5OUTBOYVN6SXlSRkpXTWtGVFRWWkdVRlphVWxaSVUwdENOMWRDVkRkTlIwcFVVVVJJVURWUFJFbFNXVkU9fHOS1OLu7icoPYqb0epqQTFFvCNCmug7f7t_IVGeW2dz';
+let baseURL = "https://gateway.xoniaapp.com/api/";
+let sessionToken = "your_token";
 
-module.exports = {
-    baseURL,
-    sessionToken
-}
+module.exports = { baseURL, sessionToken };
