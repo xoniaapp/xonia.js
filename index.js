4d074f934c639ffd65b6ffb68143fb268c6f56b5
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-12 00:03:20 +0600

    examples coming soon

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 // Example WebHook
-const { sendMessage } = require("xonia.js");
+const { sendMessage } = require("./src");
+
 sendMessage("**Hi, there.**", "channelID", "sessionToken");
+
+// Weather Application WebHook
+const { sendMessage } = require("./src");
+
+sendMessage("Location: Degree:"); // Get the weather data from an APIand send the response.
+// async await is valid as well.
+
+// News WebHook
+// Same as weather hook
